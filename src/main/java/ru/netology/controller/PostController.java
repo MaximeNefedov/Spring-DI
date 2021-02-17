dccbb93ccf6094e543a627b16b469e0f9685c748
Maxim Nefedov <max-ipad@mail.ru>
2021-02-17 18:42:04 +0300

    Initial commit

---
+++ src/main/java/ru/netology/controller/PostController.java
@@ -0,0 +1,54 @@
+package ru.netology.controller;
+
+import com.google.gson.Gson;
+import ru.netology.model.Post;
+import ru.netology.service.PostService;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.all();
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var post = service.getById(id);
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(post));
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+        if (data != null) {
+            response.getWriter().print(gson.toJson(data));
+        } else {
+            response.getWriter().println("Сохранение не удалось");
+        }
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        int status;
+        if (service.removeById(id)) {
+            status = HttpServletResponse.SC_OK;
+        } else {
+            status = HttpServletResponse.SC_NOT_FOUND;
+        }
+        response.getWriter().print(status);
+    }
+}
---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,58 @@
+package ru.netology.repository;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PostRepository {
+    private Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    public PostRepository() {
+        Post defaultPost = new Post(1L, "Hello");
+        addPost(defaultPost.getId(), defaultPost);
+        counter.getAndIncrement();
+        Post defaultPost2 = new Post(2L, "World");
+        addPost(defaultPost2.getId(), defaultPost2);
+        counter.getAndIncrement();
+    }
+
+    private void addPost(long id, Post post) {
+        posts.put(id, post);
+    }
+
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        Post post = posts.get(id);
+        return Optional.ofNullable(post);
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+//            Создание нового поста
+            post.setId(counter.incrementAndGet());
+            addPost(post.getId(), post);
+            return post;
+        } else if (post.getId() != 0) {
+            if (posts.containsKey(post.getId())) {
+                Post changedPost = posts.get(post.getId());
+                changedPost.setContent(post.getContent());
+                return changedPost;
+            }
+        }
+        return null;
+    }
+
+    public boolean removeById(long id) {
+        if (posts.get(id) != null) {
+            posts.remove(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
