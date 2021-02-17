e728b8ad2edc58db2cd68f102b35903788494a75
Maxim Nefedov <max-ipad@mail.ru>
2021-02-17 19:07:43 +0300

    Add DI annotations

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,16 +1,20 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 import jakarta.servlet.http.*;
 import java.io.IOException;
 import java.io.Reader;
 
+@Controller
 public class PostController {
     public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
 
+    @Autowired
     public PostController(PostService service) {
         this.service = service;
     }
